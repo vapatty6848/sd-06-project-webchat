f96399397f3b2c1b5774d5b15e483005fa8b5141
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-04-14 15:34:20 -0300

    Iniciando o projeto

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+
+app.use(express.json());
+
+io.on('connection', (socket) => {
+  console.log(`Usuário ${socket.id} conectado`);
+});
+
+app.set('view engine', 'ejs');
+app.get('/', (_req, res) => {
+  res.render('index');
+});
+
+httpServer.listen(3000);
