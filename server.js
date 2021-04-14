10dc52ba4a1dcecc39c6f1312d49236eb7c1e219
isabellavjs <isabellavjs@gmail.com>
2021-04-14 16:51:12 -0300

    First commit

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.set('view engine', 'ejs');
+
+const port = 3000;
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log(`Usuário ${socket.id} conectado`);
+});
+
+app.get('/', (_req, res) => {
+  res.render('home/index');
+});
+
+app.listen(port, () => console.log(`Example app listening on ${port}!`));
