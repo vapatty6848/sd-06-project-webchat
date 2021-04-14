29633e0724a863bd814be8c67b7374f2755ca03b
DehSartoreto <fixsartoreto@gmail.com>
2021-04-14 16:53:00 -0300

    Req 1 deu bom?!

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 const cors = require('cors');
 
@@ -9,14 +11,26 @@ const io = require('socket.io')(http, {
   },
 });
 
-app.use(cors());
+const { addMessages } = require('./models/messages');
 
-app.get('/', (req, res) => {
-  res.sendFile('/index.html');
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', async (_req, res) => {
+  res.render('../views/');
 });
 
 io.on('connection', (socket) => {
-  console.log('Usuário conectado');
+  console.log(`User ${socket.id} connected.`);
+  socket.on('disconnect', () => console.log(`User ${socket.id} disconnected.`));
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const messageTime = new Date();
+    await addMessages({ chatMessage, nickname, messageTime });
+    const result = `${messageTime} - ${nickname} - ${chatMessage}`;
+    io.emit('message', result);
+  });
 });
 
 http.listen(3000, () => {
