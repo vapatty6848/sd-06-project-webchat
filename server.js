f69d352929ecaec76353948d7fe20bfd2b1f21a2
DehSartoreto <fixsartoreto@gmail.com>
2021-04-14 15:45:16 -0300

    Webchatting

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('Usuário conectado');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
