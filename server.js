288f2c7d94a81077482d5ce55181bd7a75771e4c
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-04-14 19:51:04 -0300

    req 1

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
-const moment = require('moment');
 const io = require('socket.io')(httpServer);
 
-app.use(express.json());
-
-io.on('connection', (socket) => {
-  console.log(`Usuário ${socket.id} conectou!`);
-
-  socket.on('message', (obj) =>{
-    console.log(`${moment().format('DD-MM-yyyy hh:mm')} - ${obj.nickname}: ${obj.chatMessage}`);
-  })
-});
+io.on('connection', (socket) => console.log(`Usuário ${socket.id} conectou!`));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.get('/', (_req, res) => res.render('home'));
 
-httpServer.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
-});
+httpServer.listen(3000, () => console.log('Servidor ouvindo na porta 3000'));
