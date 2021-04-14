b6f6635366f459cfecef60256a6d046d744986d8
isabellavjs <isabellavjs@gmail.com>
2021-04-14 20:02:15 -0300

    Add req 1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    method: ['GET', 'POST'],
   },
 });
 
@@ -17,10 +17,15 @@ app.use(cors());
 
 io.on('connection', (socket) => {
   console.log(`Usuário ${socket.id} conectado`);
+
+  socket.on('message', (data) => {
+    // console.log('mensagem usuario', data);
+    io.emit('message', data);
+  });
 });
 
 app.get('/', (_req, res) => {
   res.render('home/index');
 });
 
-app.listen(port, () => console.log(`Example app listening on ${port}!`));
+httpServer.listen(port, () => console.log(`Example app listening on ${port}!`));
