0c3680a62be6c49ed1f8ff8fd1f24132e572dab0
danieleperse <danieleperse3@gmail.com>
2021-04-14 19:53:37 -0300

    subindo codigo do dia

--- server.js
+++ server.js
@@ -14,9 +14,24 @@ const io = require('socket.io')(httpServer, {
 
 app.use(cors());
 
+const users = [];
+
 io.on('connection', (socket) => {
   console.log(`Usuário novo conectado ${socket.id}`);
-  // sentMessage
+
+  users.push({ socketId: socket.id });
+
+  io.emit('updateUsers', users);
+
+  socket.on('sentMessage', (message) => { // 2. aqui no back ele capta o que foi escrito no canal sentMessage lá do front
+    console.log(message);
+    io.emit('newMessage', message); // 3. aqui meu back, emit a mensagem captada no canal sentMessage para todos os usuarios conectados e cria outro canal, o newMessage que pode ser captado pelo front
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Usuário ${socket.id} desconectado`);
+    // io.emit('newMessage', `Usuário no socket ${socket.id} se desconectou`);
+  });
 });
 
 app.set('view engine', 'ejs');
