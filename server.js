21ae441dcaed79678bc7c63f1d17a5e6b984678d
Ana Flávia <anaflvia@gmail.com>
2021-04-14 20:43:35 -0300

    testando evaluator

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const path = require('path');
+const cors = require('cors');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+
+io.on('connection', (socket) => {
+  console.log('usuário conectado')
+})
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, '/views/index'));
+});
+
+http.listen(3000, () => console.log('Ouvindo na porta 3000'));
