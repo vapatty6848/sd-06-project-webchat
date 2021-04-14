fbab1c454859e0855730585c4b919adc7d1a7492
William Gomes <williamjog@hotmail.com>
2021-04-14 15:46:31 -0300

    1º Requisito - Incompleto

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.set('view engine', 'ejs'); // Informa para o app que será utilizado ejs para montar a página.
+
+app.set('views', './views');
+
+const PORT = 3000;
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+app.get('/', (request, response) => {
+  response.render('../views/');
+});
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} has connected.`);
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} has disconnected.`);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server has been started on Port ${PORT}.`);
+});
