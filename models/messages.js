c905a9fb65cca64ababd41dbdeffde4a7a49b044
danieleperse <danieleperse3@gmail.com>
2021-04-14 17:58:18 -0300

    inicio conexao socketio

---
+++ models/messages.js
@@ -0,0 +1,24 @@
+const connection = require('./connection');
+
+const create = async (nickname, message) => {
+  const newMessage = await connection()
+    .then((db) => db.collection('messages').insertOne({
+      nickname,
+      message,
+      timestamp: Date.now(),
+    }));
+
+  return newMessage;
+};
+
+const getAll = async () => {
+  const allMessages = await connection()
+    .then((db) => db.collection('messages').find().toArray());
+  
+  return allMessages;
+};
+
+module.exports = {
+  create,
+  getAll,
+};
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const cors = require('cors');
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log(`Usuário novo conectado ${socket.id}`);
+  // sentMessage
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('homeView');
+});
+
+httpServer.listen('3000');
