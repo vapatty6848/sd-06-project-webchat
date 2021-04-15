c037306b8ddafd9a61df280ca671c91fd44a3f3d
Paulo Frazão <paulo4187@gmail.com>
2021-04-15 16:01:30 -0300

    Starting Project

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const PORT = 3000;
+const io = require('socket.io')(httpServer);
+
+io.on('connection', (socket) => {
+console.log(`Usuário novo conectado ${socket.id}`);
+});
+app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+app.get('/', (_req, res) => res.render('home'));
+
+httpServer.listen(PORT, () => console.log(`Escutando a porta ${PORT}`));
