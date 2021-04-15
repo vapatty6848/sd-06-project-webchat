861cc7fa3577243e6b2d9ec0828a9fa833dd76e2
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-04-15 19:02:43 -0300

    Requirement 01

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
 // Faça seu código aqui
 const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'https://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+ io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = new Date();
+    const d = date.getDay();
+    const mo = date.getMonth();
+    const y = date.getFullYear();
+    const h = date.getHours();
+    const mi = date.getMinutes();
+    const s = date.getSeconds();
+
+    const message = `${d}-${mo}-${y} ${h}:${mi}:${s} - ${nickname}: ${chatMessage} `;
+
+    io.emit('message', message);
+  });
+ });
+
+ httpServer.listen(3000);
