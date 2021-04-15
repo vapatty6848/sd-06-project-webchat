e93aefb5a3589ac32068b5affccbaa163478fd60
mottak <kissylamottapereira@gmail.com>
2021-04-15 17:45:28 -0300

    primeiro commit web chat

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'https://localhost3000',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/client.html');
+// });
+
+io.on('connection', (socket) => {
+  console.log('usuário conectado');
+
+  socket.on('disconnect', () => {
+    console.log(
+      'usuário desconectado'
+    );
+});
+
+http.listen(3000, () => console.log('Servidor na porta 3000'));
