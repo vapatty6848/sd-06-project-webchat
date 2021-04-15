29118fd93a69a653d217beef4f3a3f0ec7604815
LeandroParisi <lepcbelisario@gmail.com>
2021-04-15 15:31:08 -0300

    setup: creates MVC folder structure

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Faça seu código aqui
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(socket);
+  console.log('connected');
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'ok' });
+});
+
+http.listen(3000, () => {
+  console.log('Server on port 3000');
+});
