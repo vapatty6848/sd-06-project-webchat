cb5ea32bc3c2f2f271efb77d65a6abce733de8da
bynho25 <pablo.trybe@gmail.com>
2021-04-14 22:34:51 -0300

    Starting the chat. Req 1

---
+++ models/message.js
@@ -0,0 +1,24 @@
+const connection = require('./connection');
+
+const createMessage = async (message, nickname) => {
+  const newMessage = await connection()
+    .then((db) => db.collection('message').insertOne({
+      nickname,
+      message,
+      timestamp: Date.now();
+    }));
+
+  return newMessage;
+};
+
+const getAllMessages = async () => {
+  const allMessages = await connection()
+    .then((db) => db.collection('message').find().toArray());
+
+  return allMessages;
+};
+
+module.exports = {
+  createMessage,
+  getAllMessages,
+};
--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log(`Conectado ${socket.id}`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('view');
+});
+
+httpServer.listen('3000');
