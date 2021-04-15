7ee0966fc76ff7a70848254fdf8bb867d748717b
ricardorosa-dev <ricardorosa@gmail.com>
2021-04-15 13:17:14 -0300

    Lint no CSS??

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,9 +2,6 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  
-  return res.status(200).render('chatView');
-});
+router.get('/', async (req, res) => res.status(200).render('chatView'));
 
 module.exports = router;
