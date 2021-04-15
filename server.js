2a4b5f86f96efa34523d8b9bf84bcdbd8c44c057
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-04-15 20:02:36 -0300

    first attempts at sending messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const cors = require('cors');
 const randomize = require('randomatic');
+const { format } = require('date-fns');
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000',
@@ -11,14 +12,18 @@ const io = require('socket.io')(http, {
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 
-app.use(cors);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(cors());
 
 io.on('connection', (socket) => {
-  socket.on('message', (message) => console.log('fist message: ', message));
+  socket.on('message', (message) => {
+    const { chatMessage, nickname } = message;
+    const formattedDate = format(new Date(), 'dd-MM-yyyy KK:mm:ss aa');
+    const formattedMessage = `${formattedDate} - ${nickname}: ${chatMessage}`;
+    io.emit('serverMessage', formattedMessage);
+  });
 });
-
-app.set('views engine', 'ejs');
-app.set('views', './views');
 
 app.get('/', (_req, res) => {
   res.render('chathome');
