43c843aa2ba3ffefd6cde16a5543444de6d60d65
Paulo Frazão <paulo4187@gmail.com>
2021-04-15 18:28:05 -0300

    feat: requirement 01 done

--- server.js
+++ server.js
@@ -6,9 +6,26 @@ const httpServer = require('http').createServer(app);
 const PORT = 3000;
 const io = require('socket.io')(httpServer);
 
+const generateData = () => {
+  const data = new Date().toLocaleDateString('en-GB');
+  const time = new Date().toLocaleTimeString('en-GB');
+  const dateTime = (`0${data} ${time}`).replace(/[/]/g, '-');
+  return dateTime;
+};
+
 io.on('connection', (socket) => {
-console.log(`Usuário novo conectado ${socket.id}`);
+ // socket.on('NewUser', (user) => console.log(user));
+console.log(socket.id);
+
+  socket.on('message', (message) => {
+  console.log(message);
+  const { chatMessage, nickname } = message;
+  const dateTime = generateData();
+  const messageFormat = `${dateTime} - ${nickname}: ${chatMessage}`;
+    io.emit('message', messageFormat);
+  });
 });
+
 app.set('view engine', 'ejs');
 // app.set('views', './views');
 
