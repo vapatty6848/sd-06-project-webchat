be9bd3a28a6b2b263e0ed008152e6757e9f2fa6e
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-04-15 20:21:48 -0300

    pausa para a janta

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const io = require('socket.io')(httpServer);
+
+const port = 3001;
+
+io.on('connection', (socket) => {
+  socket.on('allMessage', (message) => {
+    io.emit('newMessage', message);
+  });
+});
+/**
+ * io.emit manda pra todos
+ * socket.emit manda para um específico
+ * broadcast.emit manda para todos menos para que enviou
+ */
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+httpServer.listen(port, () => console.log(`escutando a porta ${port}!`));
