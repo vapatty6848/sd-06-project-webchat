df8c9be2d4ad546d7987a87d68a47c795b5b9fd8
StefanoBranz <stefano.branz@gmail.com>
2021-04-16 08:06:24 -0300

    Requirement 01 push 05

--- server.js
+++ server.js
@@ -23,37 +23,44 @@ app.get('/', (_req, res) => {
 let users = [];
 const date = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss TT');
 
-// DD-MM-yyyy HH:mm:ss
-
-//  const teste = (socket) => () => {
-//   users.push({ socketId: socket.id, name: `Guest_${socket.id}` });
-
-//   io.emit('updateUsers', users);
-//   io.emit('logStatus', `Usuário ${socket.id} conectou`);
-// };
-
+// eslint-disable-next-line max-lines-per-function
 io.on('connection', (socket) => {
-  socket.on('newUser', () => {
-    users.push({ socketId: socket.id, name: `Guest - ${socket.id}` });
+  socket.on('newUser', (nickname) => {
+    const userExists = users.find((user) => user.socketId === socket.id);
+    if (userExists) {
+      userExists.name = nickname;
+      io.emit('updateUsers', users);
+      io.emit('logStatus', `Usuário ${nickname} conectou`);
+      console.log('users do meu newUserExists', users);
+      return;
+    }
 
-  io.emit('updateUsers', users);
-  io.emit('logStatus', `Usuário ${socket.id} conectou`);
+    users.push({ socketId: socket.id, name: nickname });
+
+    io.emit('updateUsers', users);
+    io.emit('logStatus', `Usuário ${nickname} conectou`);
+    console.log('users do meu updateUsers', users);
   });
-  
+
   socket.on('message', ({ chatMessage, nickname }) => {
-   io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+    console.log('users do meu Message', users);
   });
 
   socket.on('logStatus', (logStatus) => {
     io.emit('logStatus', logStatus);
+    console.log('users do meu logStatus', users);
   });
 
   socket.on('disconnect', () => {
-    io.emit('logStatus', `Usuário ${socket.id} desconectou`);
-    // console.log(socket.id, users);
+    const disconnectingUser = users.find((user) => user.socketId === socket.id);
+    if (disconnectingUser) {
+      io.emit('logStatus', `Usuário ${disconnectingUser.name} desconectou`);
+      console.log('users do meu disconectEXISTS', users);
+    }
     users = users.filter((user) => user.socketId !== socket.id);
-    
     io.emit('updateUsers', users);
+    console.log('users do meu disconnect', users);
   });
 });
 
