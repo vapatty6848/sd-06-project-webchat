fc9a61af0060449e00f92166c36856577780cd7c
bynho25 <pablo.trybe@gmail.com>
2021-04-16 01:04:39 -0300

    Fix req 1 and 2

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-// Faça seu código aqui
-const bodyParser = require('body-parser');
-const express = require('express');
-const path = require('path');
-const dateFormat = require('dateformat');
-
-const app = express();
+const app = require('express')();
 const httpServer = require('http').createServer(app);
+const dateFormat = require('dateformat');
+// const path = require('path');
 const cors = require('cors');
-
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: 'http://localhost:3000',
@@ -15,37 +10,69 @@ const io = require('socket.io')(httpServer, {
   },
 });
 
-const { createMessage, getAllMessages } = require('./models/message');
-
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(bodyParser.json());
-app.use(cors());
+app.get('/', (req, res) => {
+  // res.render(path.join(__dirname, '/views/index'));
+  res.render('index');
+});
+
+const users = [];
+
+const addNewUser = (socket) => {
+  const newUser = {
+    id: socket.id,
+  };
+  users.push(newUser);
+};
+
+const addNickname = (nickname, socket) => {
+  const index = users.findIndex((user) => user.id === socket.id);
+  users[index].nickname = nickname;
+};
+
+const changeNickname = (newNickname, socket) => {
+  const index = users.findIndex((user) => user.id === socket.id);
+  users[index].nickname = newNickname;
+};
+
+const getNickname = (socket) => {
+  const index = users.findIndex((user) => user.id === socket.id);
+  return users[index].nickname;
+};
+
+const randomNickname = () => {
+  const size = 16;
+  const randomCharacters = 'ABCDefghiJKLmNoPQRStuv125478963';
+  let randomic = '';
+  for (let i = 0; i < size; i += 1) {
+    randomic += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
+  }
+  return randomic;
+};
 
 io.on('connection', (socket) => {
-  console.log(`Conectado ${socket.id}`);
-
-  io.emit('conectado', `${socket.id}`);
-
-  socket.on('message', async ({ nickname, message }) => {
+  console.log(`${socket.id} connected`);
+  addNewUser(socket);
+  console.log(users);
+  io.emit('connected', randomNickname());
+  socket.on('message', ({ chatMessage, nickname }) => {
+    addNickname(nickname, socket);
     const dateTime = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss TT');
-    await createMessage(nickname, message, dateTime);
-    const newMessage = `${dateTime} - ${nickname}: ${message}`;
-    io.emit('message', newMessage);
+    const response = `${dateTime} ${getNickname(socket)} ${chatMessage}`;
+    io.emit('message', response);
   });
 
-  // socket.on('disconnect', () => {
-  //   console.log(`${socket.id} desconectado`);
-  // });
+  socket.on('changeNickname', ({ newNickname }) => {
+    changeNickname(newNickname, socket);
+    io.emit('changeNickname', users);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected');
+  });
 });
 
-app.get('/', async (req, res) => {
-  const allMessages = await getAllMessages();
-  res.status(200).render('index', { allMessages });
-});
-
-httpServer.listen(3000, () => {
-  console.log('Servidor na porta 3000');
-});
+httpServer.listen(3000, () => console.log('Na porta 3000'));
