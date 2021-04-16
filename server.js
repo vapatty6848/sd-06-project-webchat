f5359482950ad2860c53cb9ae0098aca293d8a18
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-04-16 05:34:10 -0300

    incluindo requisitos do chat no back

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const express = require('express');
 
 const app = express();
 const httpServer = require('http').createServer(app);
+const { format } = require('date-fns');
 
-const io = require('socket.io')(httpServer);
+const cors = require('cors');
 
-const port = 3001;
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 io.on('connection', (socket) => {
-  socket.on('allMessage', (message) => {
-    io.emit('newMessage', message);
+  socket.on('message', (message) => {
+    const { chatMessage, nickname } = message;
+    const formattedDate = format(new Date(), 'dd-MM-yyyy KK:mm:ss aa');
+    const formattedMessage = `${formattedDate} - ${nickname}: ${chatMessage}`;
+    io.emit('serverMessage', formattedMessage);
   });
 });
+
 /**
  * io.emit manda pra todos
  * socket.emit manda para um específico
@@ -20,9 +32,10 @@ io.on('connection', (socket) => {
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.render('home');
 });
 
-httpServer.listen(port, () => console.log(`escutando a porta ${port}!`));
+httpServer.listen(PORT, () => console.log(`escutando a porta ${PORT}!`));
