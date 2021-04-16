d6062befd7711ba3aa6bbfd9197d689abae767ed
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-04-16 20:09:16 -0300

    css quase pronto

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -4,22 +4,9 @@ const messagesModel = require('../models/messagesModel');
 
 const router = Router();
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const { message } = req.body;
-
-//     const newMessage = await messagesModel.create(message);
-
-//     return res.status(200).send(newMessage);
-//   } catch (e) {
-//     return res.status(500).json({ message: 'internal error' });
-//   }
-// });
-
 router.get('/', async (_req, res) => {
   try {
     const arrayOfMessages = await messagesModel.getAll();
-    console.log(arrayOfMessages);
 
     const messages = arrayOfMessages.map((msg) => {
       const date = moment(msg.timestamp).format('DD-MM-yyyy hh:mm:ss');
