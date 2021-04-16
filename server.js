705bc4cda9f6469d35b712603e6495c339d1f5e6
Daniel Moura <darthur.moura@gmail.com>
2021-04-15 21:29:10 -0300

    First configs> home.ejs server.js msging sockets

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-// Faça seu código aqui
+const express = require('express');
+const dayjs = require('dayjs');
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+
+let msgsArray = [];
+
+io.on('connection', (socket) => {
+  console.log(`Novo usuário! ${socket.id}`);
+  socket.on('bc-message', (data) => {
+    console.log(data);
+
+    msgsArray = [...msgsArray, data.message];
+    const date = dayjs().format('DD-MM-YYYY hh:mm:ss A');
+
+    const message = `<li>
+      <strong>${date} - ${data.userNickname}</strong>: ${data.message}
+    </li>`;
+
+    io.emit('message', message);
+  });
+});
+
+
+app.set('view engine', 'ejs');
+
+app.get('/webchat', (req, res) => {
+  res.render('home');
+});
+
+httpServer.listen('3000');
