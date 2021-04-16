aef8115318900c826c3728242dd375f1910507bc
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-04-16 13:35:21 -0300

    Req 1 e 2

--- server.js
+++ server.js
@@ -1,1 +1,50 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('page');
+  // res.sendFile(__dirname + '/');
+});
+
+const users = [];
+const beforeMessage = () => {
+  const date = new Date();
+  const formatedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+  const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+  const hour = date.getHours() % 12;
+  const formatedTime = `${hour}:${date.getMinutes()}:${date.getSeconds()} ${ampm}`;
+  return `${formatedDate} ${formatedTime}`;
+};
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+  socket.on('newUser', (user) => {
+    users.push({ socketid: socket.id, nickname: user });
+    io.emit('usersList', users);
+  });
+
+  socket.on('message', ({ nickname, chatMessage }) => {
+    console.log(chatMessage);
+    io.emit('message', `${beforeMessage()} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+    const index = users.findIndex((user) => user.socketid === socket.id);
+    users.splice(index, 1);
+    io.emit('usersList', users);
+  });
+});
+
+http.listen(3000);
