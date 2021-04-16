48223d045ed7dd8175c3157e33b9e6f33a3a5ce5
LeandroParisi <lepcbelisario@gmail.com>
2021-04-16 16:41:36 -0300

    setup: modulizes socket on client

---
+++ controllers/servi.js
@@ -0,0 +1,14 @@
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const generateChat = () => {
+  
+  io.on('connection', (socket) => {
+    console.log('connected');
+
+}
+
