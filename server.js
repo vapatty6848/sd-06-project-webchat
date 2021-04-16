ab5a7ea5cec3398be21c0785847417b104e09500
Paulo Frazão <paulo4187@gmail.com>
2021-04-16 14:40:52 -0300

    feat: views/home implementation

--- server.js
+++ server.js
@@ -13,16 +13,27 @@ const generateData = () => {
   return dateTime;
 };
 
+const users = [];
 io.on('connection', (socket) => {
- // socket.on('NewUser', (user) => console.log(user));
-console.log(socket.id);
+console.log(`${socket.id}, Conectou`);
 
-  socket.on('message', (message) => {
-  console.log(message);
-  const { chatMessage, nickname } = message;
-  const dateTime = generateData();
-  const messageFormat = `${dateTime} - ${nickname}: ${chatMessage}`;
+  socket.on('NewUser', (user) => {
+    console.log(''+ '' + user)
+    users.push({ id: `${socket.id}`, name: user });
+    io.emit('usersNick', users);
+});
+  socket.on('message', (message) => {   
+    const { chatMessage, nickname } = message;
+    const dateTime = generateData();
+    const messageFormat = `${dateTime} - ${nickname}: ${chatMessage}`;
     io.emit('message', messageFormat);
+  });
+
+  socket.on('disconnect', () => {
+    const userIndex = users.findIndex((user) => user.id === socket.id);
+    if (userIndex !== -1) users.splice(userIndex, 1);  
+    io.emit('usersNick', users);
+    console.log(`${socket.id}, Desconectou`);
   });
 });
 
