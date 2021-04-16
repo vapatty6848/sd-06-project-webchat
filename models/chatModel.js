b731e093264cf280d55a924611e90c85b3e03616
ricardorosa-dev <ricardorosa@gmail.com>
2021-04-16 17:43:17 -0300

    LINT

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,7 +1,7 @@
 const connection = require('./connection');
 
-const getAll = async() => {
-  await connection().then((db) => db.collection('messages').find().toArray());
+const getAll = async () => {
+  return connection().then((db) => db.collection('messages').find().toArray());
 };
 
 module.exports = {
