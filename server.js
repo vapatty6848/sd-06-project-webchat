82acecd265a64b3fa5ae8cd37997473996113182
FlavioSugano <flavio.sugano@gmail.com>
2021-04-17 16:18:27 -0300

    refactor: organizing listeners on server.js

--- server.js
+++ server.js
@@ -7,43 +7,51 @@ const model = require('./models/messages');
 const { createTimestamp } = require('./utils/createTimestamp');
 const formatMessage = require('./utils/formatChatMessage');
 
-const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static(`${__dirname}/public/`));
 
+const port = process.env.PORT || 3000;
 let users = [];
 
-const changeNicknameFunction = ({ newNickname, socket }) => {
+const sendNicknameListener = ({ nickname, socket }) => {
+  users.push({ id: socket.id, nickname });
+
+  io.emit('updateOnlineUsers', users);
+};
+
+const messageListener = async ({ nickname, chatMessage }) => {
+  const timestamp = createTimestamp();
+  const message = formatMessage({ nickname, chatMessage, timestamp });
+
+  await model.saveMessage({ nickname, chatMessage, timestamp });
+
+  io.emit('message', message);
+};
+
+const changeNicknameListener = ({ newNickname, socket }) => {
   users.map((user) => {
     if (user.id === socket.id) Object.assign(user, { id: user.id, nickname: newNickname });
 
     return user;
   });
+
   io.emit('updateOnlineUsers', users);
 };
 
-const disconnectFunction = (socket) => {
+const disconnectListener = (socket) => {
   const onlineUsers = users.filter((user) => user.id !== socket.id);
+
   users = onlineUsers;
+  
   io.emit('updateOnlineUsers', users);
 };
 
 io.on('connection', async (socket) => {  
-  socket.on('sendNickname', ({ nickname }) => {
-    users.push({ id: socket.id, nickname });
-    io.emit('updateOnlineUsers', users);
-  });
-
-  socket.on('message', async ({ nickname, chatMessage }) => {
-    const timestamp = createTimestamp();
-    await model.saveMessage({ nickname, chatMessage, timestamp });
-    io.emit('message', formatMessage({ nickname, chatMessage, timestamp }));
-  });
-
-  socket.on('changeNickname', (newNickname) => changeNicknameFunction({ newNickname, socket }));
-
-  socket.on('disconnect', () => disconnectFunction(socket));
+  socket.on('sendNickname', ({ nickname }) => sendNicknameListener({ nickname, socket }));
+  socket.on('message', ({ nickname, chatMessage }) => messageListener({ nickname, chatMessage }));
+  socket.on('changeNickname', (newNickname) => changeNicknameListener({ newNickname, socket }));
+  socket.on('disconnect', () => disconnectListener(socket));
 });
 
 app.get('/', async (_req, res) => {
