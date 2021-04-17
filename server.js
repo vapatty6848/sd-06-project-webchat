36df89ecaa2596321dd42a1d3025dfba63bdd917
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-04-16 21:54:05 -0300

    req 1 done, req 2 partial

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+// obs: add cors depois
+const io = require('socket.io')(httpServer);
+
+const users = [];
+
+const dateTime = new Date().toLocaleString().replace(/\//g, '-');
+
+io.on('connection', (socket) => {
+  socket.on('nickName', (nickName) => {
+    users.splice(0, 0, nickName);
+    io.emit('newNickName', users);
+  }); 
+  socket.on('message', (message) => {
+    io.emit('message', `${dateTime} - ${message.nickname}: ${message.chatMessage}`);
+  });
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (_req, res) => res.render('home'));
+
+httpServer.listen('3000');
