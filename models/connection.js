7c40c141fd36adada00403b54d08193233948f53
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-04-16 21:31:40 -0300

    Update: Testing return db after opening. Last test with mongoose

--- models/connection.js
+++ models/connection.js
@@ -15,7 +15,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Db Connected');
-  return 'qualquercoisa';
+  return db;
 });
 
 module.exports = connection;
