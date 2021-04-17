a1432e31a46ffb4862a3cf85d94d0ffbc208d37e
FlavioSugano <flavio.sugano@gmail.com>
2021-04-16 21:27:40 -0300

    feat: requirement 3, refactor of changeNickName and disconnect functions

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
+const model = require('./models/messages');
 const { createTimestamp } = require('./utils/createTimestamp');
+const formatMessage = require('./utils/formatChatMessage');
 
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
@@ -12,31 +14,41 @@ app.use(express.static(`${__dirname}/public/`));
 
 let users = [];
 
-io.on('connection', (socket) => {
+const changeNicknameFunction = ({ newNickname, socket }) => {
+  users.map((user) => {
+    if (user.id === socket.id) Object.assign(user, { id: user.id, nickname: newNickname });
+
+    return user;
+  });
+  io.emit('updateOnlineUsers', users);
+};
+
+const disconnectFunction = (socket) => {
+  const onlineUsers = users.filter((user) => user.id !== socket.id);
+  users = onlineUsers;
+  io.emit('updateOnlineUsers', users);
+};
+
+io.on('connection', async (socket) => {
   const newUser = { id: socket.id, nickname: `user_${Math.random().toString().substr(2, 11)}` };
   users.push(newUser);
   io.emit('updateOnlineUsers', users);
 
-  socket.on('message', ({ nickname, chatMessage }) => {
-    io.emit('message', `${createTimestamp()} - ${nickname} disse: ${chatMessage}`);
+  socket.on('message', async ({ nickname, chatMessage }) => {
+    const timestamp = createTimestamp();
+    await model.saveMessage({ nickname, chatMessage, timestamp });
+    io.emit('message', formatMessage({ nickname, chatMessage, timestamp }));
   });
 
-  socket.on('changeNickname', (newNickname) => {
-    users.map((user) => {
-      if (user.id === socket.id) Object.assign(user, { id: user.id, nickname: newNickname });
+  socket.on('changeNickname', (newNickname) => changeNicknameFunction({ newNickname, socket }));
 
-      return user;
-    });
-    io.emit('updateOnlineUsers', users);
-  });
-
-  socket.on('disconnect', () => {
-    const onlineUsers = users.filter((user) => user.id !== socket.id);
-    users = onlineUsers;
-    io.emit('updateOnlineUsers', users);
-  });
+  socket.on('disconnect', () => disconnectFunction(socket));
 });
 
-app.get('/', (_req, res) => res.render('home'));
+app.get('/', async (_req, res) => {
+  const previousMessages = await model.getAll();
+  const messagesToRender = previousMessages.map((message) => formatMessage(message));
+  return res.render('home', { messagesToRender });
+});
 
 httpServer.listen(port, () => console.log(`Webchat server on port ${port}!`));
