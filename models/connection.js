5e47bfef780a74ac851fc16f68fd24fd63ae1ed0
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-04-16 21:24:07 -0300

    Update: Testing return after db opening. Last test with mongoose

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-const localUrl = 'mongodb://localhost:27017/webchat';
-// const remoteUrl = process.env.DB_URL;
+// const localUrl = 'mongodb://localhost:27017/webchat';
+const remoteUrl = process.env.DB_URL;
 
-const connection = mongoose.connect(localUrl, {
+const connection = mongoose.connect(remoteUrl, {
   dbName: process.env.DB_NAME,
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,6 +15,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Db Connected');
+  return 'qualquercoisa';
 });
 
 module.exports = connection;
