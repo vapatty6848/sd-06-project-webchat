04a7ee16d4cd76a7e7bd0b226b53c21480299825
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-04-18 13:35:58 -0300

    Requirement 02

--- server.js
+++ server.js
@@ -3,28 +3,43 @@ const express = require('express');
 
 const app = express();
 const httpServer = require('http').createServer(app);
-
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: 'https://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
+const dateFormater = require('./utils/dateFormat');
 
- io.on('connection', (socket) => {
+const users = [];
+
+io.on('connection', (socket) => {
+  const guest = { id: socket.id, name: (`${socket.id}`).slice(0, 16) };
+  users.push(guest);
+  socket.emit('updatedUserList', users);
+  socket.broadcast.emit('updatedUserList', users);
+  socket.on('updateUser', ({ name, id }) => {
+    const index = users.indexOf(users.find((user) => user.id === id));
+    users[index].name = name;
+    socket.emit('updatedUserList', users);
+    socket.broadcast.emit('updatedUserList', users);
+  });
   socket.on('message', ({ chatMessage, nickname }) => {
     const date = new Date();
-    const d = date.getDay();
-    const mo = date.getMonth();
-    const y = date.getFullYear();
-    const h = date.getHours();
-    const mi = date.getMinutes();
-    const s = date.getSeconds();
-
-    const message = `${d}-${mo}-${y} ${h}:${mi}:${s} - ${nickname}: ${chatMessage} `;
-
-    io.emit('message', message);
+    const brDate = dateFormater(date);
+    const person = users.find((user) => user.id === nickname);
+    const message = `${brDate} - ${person.name}: ${chatMessage} `;
+    io.emit('newMessage', message);
   });
- });
+});
 
- httpServer.listen(3000);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+httpServer.listen(3000, () => {
+  console.log('Chat Inicializado');
+});
