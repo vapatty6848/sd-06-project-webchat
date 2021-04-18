d292595fb38e37090c1916929cc6618ea476f033
Cássio Rubens <cassiorubensadv@gmail.com>
2021-04-18 18:38:05 -0300

    FEAT: requirement 4

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 const express = require('express');
-// const cors = require('cors');
+
 const app = express();
 const httpServer = require('http').createServer(app);
 // Roda o socket io
 const io = require('socket.io')(httpServer);
 const modelMessages = require('./models/messagesConnection');
-// const modelUser = require('./models/userConnection');
 
 const PORT = 3000;
+
+let usersConnected = [];
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// app.use(cors());
-
-// let quantity = 0;
-// app.get('/', (_req, res) => {
-//   res.render('home', { quantity });
-//   quantity += 1;
-// });
-
 const manageDate = () => {
-  // const includeZero = (n) => {
-  //   if (n <= 9) return `0${n}`;
-  //   return n;
-  // };
   const date = new Date().toLocaleDateString('en-GB');
   const hour = new Date().toLocaleTimeString('en-GB');
   const dateAndHour = (`${date} ${hour}`).replace(/[/]/g, '-');
@@ -41,32 +30,70 @@ function generatorNickname(lengthNickname) {
 }
 // 2
 
-io.on('connection', (socket) => {
-  const newUser = generatorNickname(16);
-  console.log(`usuario novo conectado ${newUser}`);
+function handleNewUser(socket) {
+  socket.on('initializeUser', () => {
+    const newNickname = generatorNickname(16);
+    const newUser = { socketId: socket.id, nickname: newNickname };
 
-  socket.emit('newUser', newUser);
+    usersConnected.unshift(newUser);
 
+    io.emit('addNewUser', newUser);
+  });
+}
+
+function handleMessage(socket) {
   socket.on('message', async (message) => {
     const { chatMessage, nickname } = message;
     const dateAndHour = manageDate();
     const formattedMessage = `${dateAndHour} - ${nickname}: ${chatMessage}`;
-    console.log(formattedMessage);
     await modelMessages.saveMessage({ chatMessage, nickname, dateAndHour });
     io.emit('message', formattedMessage);
   });
+}
+
+function handleNewNickname(socket) {
+  socket.on('setNewNickname', (oldNickname, newNickname) => {
+    usersConnected = usersConnected.map((user) => {
+      const userUpdated = user;
+      if (userUpdated.socketId === socket.id) {
+        userUpdated.nickname = newNickname;
+      }
+      return userUpdated;
+    });
+    const updatedUser = { oldNickname, newNickname };
+    io.emit('updatedUserNickname', updatedUser);
+  });
+}
+
+function handleDisconnectUser(socket) {
+  socket.on('disconnect', () => {
+    const userDisconnected = usersConnected.find((user) => user.socketId === socket.id);
+    usersConnected = usersConnected.filter((user) => user.socketId !== socket.id);
+    io.emit('disconnectUser', userDisconnected);
+  });
+}
+
+io.on('connection', (socket) => {
+  socket.emit('usersConnected', usersConnected);
+  
+  handleNewUser(socket);
+
+  handleMessage(socket);
+
+  handleNewNickname(socket);
+
+  handleDisconnectUser(socket);
 });
 
 // 3
-let quantity = 0;
 app.get('/', async (_req, res) => {
   const getAllMessages = await modelMessages.messagesGetAll();
   const messagesMapFormat = getAllMessages.map((e) =>
-    `${e.dateAndHour} - ${e.nickname}: ${e.chatMessage}`);
+  `${e.dateAndHour} - ${e.nickname}: ${e.chatMessage}`);
   // console.log('messagesToRender', messagesMapFormat);
-  const nicknameMapFormat = getAllMessages.map((e) => e.nickname);
-  quantity += 1;
-  return res.render('home', { quantity, messagesMapFormat, nicknameMapFormat });
+  // const nicknameMapFormat = getAllMessages.map((e) => e.nickname);
+  const nicknameMapFormat = [];// usersConnected.map((user) => user.nickname);
+  return res.render('home', { messagesMapFormat, nicknameMapFormat });
 });
 // 3
 
