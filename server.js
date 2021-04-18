1258dd16813f496f13c025e92b621b0a622259ff
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-04-18 02:34:48 -0300

    req3

--- server.js
+++ server.js
@@ -4,27 +4,38 @@ const app = express();
 const httpServer = require('http').createServer(app);
 
 const io = require('socket.io')(httpServer);
+const Messages = require('./models/Messages');
 
 const users = [];
 
 const dateTime = new Date().toLocaleString().replace(/\//g, '-');
 
-const onConnect = (socket) => {
+const onConnect = async (socket) => {
   const randomNick = `User-${Math.random().toString(36).substr(2, 16)}`;
   users.push({ id: socket.id, nickName: randomNick });
+  const messages = await Messages.getAllMessages();
   io.emit('nickNameUpdate', users);
+  socket.emit('message', messages);
   };
-
+const nickUpdate = (socket) => socket.on('nickNameUpdate', (nickName) => {
+  const currentUser = users.find((usr) => usr.id === socket.id);
+  const userIndex = users.indexOf(currentUser);
+  users.splice(userIndex, 1, { id: socket.id, nickName });
+  io.emit('nickNameUpdate', users);
+}); 
 io.on('connection', async (socket) => {
   onConnect(socket);
-  socket.on('nickNameUpdate', (nickName) => {
-    const currentUser = users.find((usr) => usr.id === socket.id);
-    const userIndex = users.indexOf(currentUser);
-    users.splice(userIndex, 1, { id: socket.id, nickName });
-    io.emit('nickNameUpdate', users);
-  }); 
-  socket.on('message', (message) => {
-    io.emit('message', `${dateTime} - ${message.nickname}: ${message.chatMessage}`);
+  nickUpdate(socket);
+   socket.on('message', async (message) => {
+    // gravar no banco
+    // enviar para o front a lista de mensagens 
+    const newMessage = { 
+      nickname: message.nickname,
+      message: message.chatMessage, 
+      date: dateTime,
+    };
+    await Messages.createMessage(newMessage.nickname, newMessage.message, newMessage.date);
+    io.emit('message', [newMessage]);
   });
   socket.on('disconnect', () => {
     const currentUser = users.find((usr) => usr.id === socket.id);
