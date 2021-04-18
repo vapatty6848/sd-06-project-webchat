fb3e005582c9d1cac09a8fe5632ed04dbcb44460
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-04-18 03:35:39 -0300

    corrections

--- server.js
+++ server.js
@@ -15,34 +15,40 @@ const onConnect = async (socket) => {
   users.push({ id: socket.id, nickName: randomNick });
   const messages = await Messages.getAllMessages();
   io.emit('nickNameUpdate', users);
-  socket.emit('message', messages);
-  };
-const nickUpdate = (socket) => socket.on('nickNameUpdate', (nickName) => {
+  messages.forEach((element) => socket.emit('message',
+   `${element.date} - ${element.nickname}: ${element.message}`)); 
+};
+
+const nickUpdate = (socket) => {
+ socket.on('nickNameUpdate', (nickName) => {
   const currentUser = users.find((usr) => usr.id === socket.id);
   const userIndex = users.indexOf(currentUser);
   users.splice(userIndex, 1, { id: socket.id, nickName });
   io.emit('nickNameUpdate', users);
-}); 
-io.on('connection', async (socket) => {
-  onConnect(socket);
-  nickUpdate(socket);
-   socket.on('message', async (message) => {
-    // gravar no banco
-    // enviar para o front a lista de mensagens 
-    const newMessage = { 
-      nickname: message.nickname,
-      message: message.chatMessage, 
-      date: dateTime,
-    };
-    await Messages.createMessage(newMessage.nickname, newMessage.message, newMessage.date);
-    io.emit('message', [newMessage]);
+  }); 
+};
+
+const messageProcess = async (socket) => {
+  socket.on('message', async (message) => {
+    const { nickname, chatMessage } = message;
+    await Messages.createMessage(nickname, chatMessage, dateTime);
+    io.emit('message', `${dateTime} - ${nickname}: ${chatMessage}`);
   });
+};
+
+const onDisconnect = async (socket) => {
   socket.on('disconnect', () => {
     const currentUser = users.find((usr) => usr.id === socket.id);
     const userIndex = users.indexOf(currentUser);
     users.splice(userIndex, 1);
     io.emit('nickNameUpdate', users);
   });
+};
+io.on('connection', async (socket) => {
+  onConnect(socket);
+  nickUpdate(socket);
+  messageProcess(socket);
+  onDisconnect(socket);
 });
 
 app.set('view engine', 'ejs');
