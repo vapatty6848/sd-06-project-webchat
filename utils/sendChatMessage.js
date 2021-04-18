9b390a0abb427ca2cbad1f9eee7a640ac5705bd4
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-04-17 23:30:31 -0300

    fix: correct socket io event name

--- utils/sendChatMessage.js
+++ utils/sendChatMessage.js
@@ -4,7 +4,7 @@ function sendChatMessage({ chatMessage, nickname, io }) {
   const timestamp = MOMENT().format('DD-MM-YYYY HH:mm:ss');
   const message = `${timestamp} - ${nickname}: ${chatMessage}`;
   console.log(message);
-  io.emit('server.chatMessage', message);
+  io.emit('message', message);
 }
 
 module.exports = sendChatMessage;
