b8189ce7ff34bcef7072bc1b0830eb581ede4736
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-04-18 16:39:43 -0300

    :no_mouth: seguindo exemplo da aula 13/04 - socket.io renderizando uma view

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(httpServer);
+
+app.set('view engine', 'ejs');
+app.set('views', './view');
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+httpServer.listen(PORT, () => console.log(`on PORT ${PORT}`));
