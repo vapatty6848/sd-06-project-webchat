4c1483e79fa85a266eed8a1ba1ce266da794272e
LughWalle <civalfran1@gmail.com>
2021-04-19 10:14:01 -0300

    algo de errado não esta certo

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-// Faça seu código aqui novo app
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+// app.use(express.static(`${__dirname}/public/`));
+
+io.on('connection', async (socket) => {
+  console.log('conectado');
+  socket.on('disconnect', () => {
+    console.log('Desconectado');
+  });
+});
+
+app.get('/', (req, res) => res.render(`${__dirname}/views/chat.ejs`));
+http.listen(PORT, () => console.log(`webchat port: ${PORT}!`));
