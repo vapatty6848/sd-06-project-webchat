49d4d96117c4d72770b4319d3664e9aff6879b4d
thalesgr <thalesgr@gmail.com>
2021-04-19 14:36:50 -0300

    done local

--- models/messages.js
+++ models/messages.js
@@ -6,6 +6,12 @@ const createMessage = async (data) => connection()
   return newMessage.ops[0];
 });
 
+const getAll = async () => {
+  const allMsgs = await connection().then((db) => db.collection('messages').find().toArray());
+  return allMsgs;
+};
+
 module.exports = {
   createMessage,
+  getAll,
 };
--- server.js
+++ server.js
@@ -1,54 +1,74 @@
-// Faça seu código aqui
 const express = require('express');
-
-const app = express();
-const httpServer = require('http').createServer(app);
 const cors = require('cors');
-const moment = require('moment');
-const io = require('socket.io')(httpServer, {
+const path = require('path');
+const moment= require('moment');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000', // url aceita pelo cors
     methods: ['GET', 'POST'], // Métodos aceitos pela url
   },
 });
 
-const { createMessage } = require('./models/messages');
-
-const PORT = 3000;
-
 app.use(cors());
+
+const { createMessage, getAll } = require('./models/messages');
+
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(`${__dirname}/views/`));
 
 let onlineUsers = [];
 
+const addNewUser = ({ nickname, socket }) => {
+  onlineUsers.push({ id: socket.id, nickname });
+  console.log(onlineUsers);
+
+  io.emit('updateOnlineUsers', onlineUsers);
+};
+
+const changeNickname = ({ newNickname, socket }) => {
+  onlineUsers.map((user) => {
+    if (user.id === socket.id) Object.assign(user, { id: user.id, nickname: newNickname });
+
+    return user;
+  });
+
+  io.emit('updateOnlineUsers', onlineUsers);
+};
+
+
+const handleChatMessage = async ({ nickname, chatMessage }) => {
+  const timestamp = moment().format('DD-MM-YYYY hh:mm:ss A');
+  const result = `${timestamp} ${nickname} ${chatMessage}`;
+  await createMessage({ nickname, chatMessage, timestamp });
+  io.emit('message', result);
+};
+
+io.on('connection', async (socket) => {
+  socket.on('newUser', ({ nickname }) => addNewUser({ nickname, socket }));
+
+  socket.on('message', async ({ nickname, chatMessage }) =>
+    handleChatMessage({ nickname, chatMessage }));
+
+    socket.on('changeNickname', (newNickname) => changeNickname({ newNickname, socket }));
+
+  socket.on('disconnect', () => {
+    const onlineUsers = onlineUsers.filter((user) => user.id !== socket.id);
+    onlineUsers = onlineUsers;
+    io.emit('updateOnlineUsers', onlineUsers);
+  });
+});
+
 app.get('/', async (_req, res) => {
-  res.render('../views/');
+  const msgs = await getAll();
+  const messages = msgs.map((message) => 
+    `${message.timestamp} ${message.nickname} ${message.chatMessage}`);
+  return res.render('index', { messages });
 });
 
-io.on('connection', (socket) => {
-  const socketId = socket.id;
-  const random = `random${socket.id}`;
-  console.log(`User ${socketId} connected.`);
-  onlineUsers.unshift({ socketId, nickname: random });
-  io.emit('connected', { socketId, nickname: random });
-  
-  socket.on('disconnect', () => console.log(`User ${socket.id} disconnected.`));
-
-  socket.on('message', async ({ chatMessage, nickname }) => {
-    const timestamp = moment().format('DD-MM-YYYY hh:mm:ss A');
-    await createMessage({ chatMessage, nickname, timestamp });
-    const message = `${timestamp} - ${nickname}: ${chatMessage}`;
-    io.emit('message', message);
-  });
-
-  socket.on('changeNickname', (nickname) => {
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-    onlineUsers.push({ socketId, nickname });
-    io.emit('changeNickname', { socketId, nickname });
-  });
-});
-
-httpServer.listen(PORT, () => {
-  console.log(`Servidor ouvindo na porta ${PORT}`);
+http.listen(3000, () => {
+  console.log('Servidor online na porta 3000');
 });
