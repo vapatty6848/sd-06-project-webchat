011fd35651c09e9b9c317d7113d5b9fc95d28901
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-04-19 20:02:06 -0300

    feat: Requirement 01

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+const express = require('express');
+
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+const app = express();
+app.use(express.static(`${__dirname}/public/`));
+
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+const { createTimestamp } = require('./utils/TimeStamp');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Socket Connection
+io.on('connection', (socket) => {
+  console.log(`Id do usúario conectado: ${socket.id}`);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const timestamp = createTimestamp();
+
+    io.emit('message', `${timestamp} - ${nickname} disse: ${chatMessage}`);
+  });
+});
+
+app.get('/', (_req, res) => res.render('home'));
+
+httpServer.listen(PORT, () => console.log(`App Webchat listening on port ${PORT}!`));
