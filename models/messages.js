befb779f057928d63fab5cfe0cde6bfb7ffe32fc
DehSartoreto <fixsartoreto@gmail.com>
2021-04-19 10:38:05 -0300

    Req2?

--- models/messages.js
+++ models/messages.js
@@ -1,18 +1,18 @@
 const connection = require('./connection');
 
-const addMessages = async (data) => connection()
+const addMessages = async ({ chatMessage, nickname, date }) => connection()
   .then(async (db) => {
-  const newMessage = await db.collection('messages').insertOne(data);
+  const newMessage = await db.collection('messages').insertOne({ chatMessage, nickname, date });
   return newMessage.ops[0];
 });
 
-const getAllMessages = async () => connection()
+const getAllMsgs = async () => connection()
   .then(async (db) => {
     const allMessages = await db.collection('messages').find().toArray();
     return allMessages;
-});
+  });
 
 module.exports = {
   addMessages,
-  getAllMessages,
+  getAllMsgs,
 };
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const http = require('http').createServer(app);
-const cors = require('cors');
 
-app.use(express.json());
-app.use(cors());
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000', // url aceita pelo cors
@@ -14,35 +13,66 @@ const io = require('socket.io')(http, {
   },
 });
 
-app.use('/', express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }),
+);
 
-const { addMessages, getAllMessages } = require('./models/messages');
+const { addMessages, getAllMsgs } = require('./models/messages');
+
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
-const onlineUsers = [];
+const allUsers = {};
 
 io.on('connection', (socket) => {
   const userId = socket.id;
   console.log(`User ${userId} connected.`);
-  socket.on('disconnect', () => {
-    console.log(`User ${userId} disconnected.`);
-    delete onlineUsers[userId];
-    io.emit('updateUser', { onlineUsers });
+
+  const allMsgs = getAllMsgs();
+  const saveMessages = [];
+
+  allMsgs.map((msg) => {
+    const { chatMessage, nickname, date } = msg;
+    return saveMessages.push(`${date} - ${nickname} - ${chatMessage}`);
+  });
+  io.emit('history', saveMessages);
+
+  socket.on('message', async ({ nickname, chatMessage }) => {
+    const date = new Date();
+    await addMessages(nickname, chatMessage, date);
+    const newMessage = `${date} - ${nickname}: ${chatMessage}`;
+    io.emit('message', newMessage);
   });
 
-  socket.on('message', async ({ chatMessage, nickname }) => {
-    const messageTime = new Date();
-    await addMessages({ chatMessage, nickname, messageTime });
-    const result = `${messageTime} - ${nickname} - ${chatMessage}`;
-    io.emit('message', result);
+  socket.on('changeName', async ({ nickname }) => {
+    io.emit('changeName', nickname);
+  });
+
+  socket.on('changeName', async ({ nickname }) => {
+    allUsers.push(nickname);
+    io.emit('online', allUsers);
+  });
+
+  socket.on('disconnect', () => {
+    delete allUsers[socket.id];
+    io.emit('online', allUsers);
   });
 });
 
 app.get('/', async (_req, res) => {
-  const allMsgs = await getAllMessages();
-  res.status(200).render('view', { onlineUsers, allMsgs });
+  const messages = await getAllMsgs();
+  res.status(200).render('index', { messages });
+});
+
+app.get('/chat', async (_req, res) => {
+  const messages = await getAllMsgs();
+  res.status(200).json(messages);
 });
 
 http.listen(3000, () => {
