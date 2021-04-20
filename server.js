bf5e300e690f660bd39d80cae884c7c2832b3f41
Luiz Mello <luizmacmillan@gmail.com>
2021-04-20 14:11:11 -0300

    Implementação da estrutura base do chat.

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const httpServer = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const http = httpServer.createServer(app);
+const io = socketIo(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/server.html`);
+});
+
+io.on('connection', (socket) => {
+  console.log('Conectado');
+  socket.on('disconnect', () => {
+    socket.broadcast.emit(
+      'serverMessage',
+      { message: 'Alguém acabou de se desconectar.' },
+    );
+  });
+  socket.on('message', (msg) => {
+    io.emit('serverMessage', { message: msg });
+  });
+  socket.emit('welcome', 'Seja bem vindo ao nosso Chat!');
+  socket.broadcast.emit(
+    'serverMessage',
+    { message: 'Alguém acabou de se conectar.' },
+  );
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
