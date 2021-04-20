e4671eaa2281f11c75fc0a316f2c86f200349eb3
alcantara2509 <alcantara2509@gmail.com>
2021-04-20 20:04:29 -0300

    req 1 --

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const dateFormat = require('dateformat');
+
+const cors = require('cors');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+const currDate = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss'); 
+
+io.on('connection', (socket) => {
+  console.log('Conectado');
+
+  // socket.broadcast.emit('mensagemServer', 
+  // { mensagem: `${socket.id} acabou de entrar` });
+
+  // socket.on('disconnect', () => {
+  //   socket.broadcast.emit('mensagemServer', 
+  // { mensagem: `${socket.id} acabou de sair` });
+  // });
+
+  socket.on('message', (msg) => {
+    io.emit('message', `${currDate} ${socket.id}: ${msg}`);
+  });
+});
+
+http.listen(3000);
