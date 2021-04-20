09767fa585e47a3bd3c910f3c10789d5178542a4
LeandroParisi <lepcbelisario@gmail.com>
2021-04-20 17:43:46 -0300

    feat: req 1 done -  lint resolvido

--- controllers/servi.js
+++ controllers/servi.js
@@ -1,13 +1,13 @@
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//   },
+// });
 
-const generateChat = () => {
+// const generateChat = () => {
   
-  io.on('connection', (socket) => {
-    console.log('connected');
-  }
-};
+//   io.on('connection', (socket) => {
+//     console.log('connected');
+//   }
+// };
