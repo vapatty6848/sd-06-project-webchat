d1ff4d92f951fbf53e5a1173ef349025881a5385
alcantara2509 <alcantara2509@gmail.com>
2021-04-21 21:30:51 -0300

    req 4 --

--- server.js
+++ server.js
@@ -12,33 +12,36 @@ const io = require('socket.io')(http, {
 
 app.use(cors());
 
+const { indexOf } = require('lodash');
 const Messages = require('./models/messageModel');
 
 app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-const currDate = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss'); 
+const currDate = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss');
+
+const allUsers = [];
 
 io.on('connection', (socket) => {
-  console.log('Conectado');
-
   socket.on('userLogin', async (nickname) => {
-    io.emit('users', nickname);
+    allUsers.push({ id: socket.id, nickname });
+    io.emit('users', allUsers);
     const allMsgs = await Messages.getAll();
     allMsgs.forEach((msg) => {
       socket.emit('message', `${msg.timestamp} ${msg.nickname}: ${msg.message}`);
     });
   });
-
+  
+  socket.on('disconnect', () => { 
+    allUsers.splice(indexOf(socket.id), 1); 
+  });
+  
   socket.on('message', async ({ chatMessage, nickname }) => {
     const dbMsg = await Messages.create(chatMessage, nickname, currDate);
     io.emit('message', `${dbMsg.timestamp} ${dbMsg.nickname}: ${dbMsg.message}`);
   });
-
-  socket.on('users', async (user) => {
-    io.emit('users', user);
-  });
+  socket.on('users', async (user) => { io.emit('users', user); });
 });
 
 http.listen(3000);
