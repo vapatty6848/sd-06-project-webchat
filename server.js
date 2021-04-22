95dbab3e47d40f1e4308e11dab952ddc6b48b306
alcantara2509 <alcantara2509@gmail.com>
2021-04-22 01:04:51 -0300

    migrate to ejs

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const app = require('express')();
 const http = require('http').createServer(app);
 const dateFormat = require('dateformat');
@@ -14,36 +15,62 @@ app.use(cors());
 
 const Messages = require('./models/messageModel');
 
-app.get('/', (_req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', async (_req, res) => {
+  const msg = await Messages.getAll();
+  res.status(200).render('index', { msg });
 });
 
 const currDate = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss');
 
-const allUsers = [];
+let allUsers = [];
+
+const newLogin = (socket, currUser) => {
+  allUsers.push(currUser);
+  socket.broadcast.emit('usersList', allUsers);
+  const filtered = allUsers.filter((el) => el !== currUser);
+  socket.emit('usersList', [currUser, ...filtered]);
+};
+
+const sendMsg = async (msg) => {
+  await Messages.create(
+    { nickname: msg.nickname, chatMessage: msg.chatMessage, timestamp: currDate },
+);
+  io.emit('message', `${currDate} - ${msg.nickname}: ${msg.chatMessage}`);
+};
 
 io.on('connection', (socket) => {
-  socket.on('userLogin', async (nickname) => {
-    allUsers.push({ id: socket.id, nickname });
-    console.log(allUsers);
-    io.emit('users', allUsers);
-    
-    const allMsgs = await Messages.getAll();
-    allMsgs.forEach((msg) => {
-      socket.emit('message', `${msg.timestamp} ${msg.nickname}: ${msg.message}`);
-    });
+  let currUser;
+  socket.on('users', (user) => {
+    currUser = user;
+    newLogin(socket, currUser);
   });
-  
-  socket.on('disconnect', () => { 
-    allUsers.splice(socket.id, 1);
-    io.emit('users', allUsers);
+
+  socket.on('disconnect', () => {
+    const updateUsers = allUsers.filter((el) => el !== currUser);
+    allUsers = updateUsers;
+    io.emit('usersList', updateUsers);
   });
-  
+
+  socket.on('updateNickname', (nickname) => {
+    const idx = allUsers.indexOf(currUser);
+    allUsers[idx] = nickname;
+    currUser = nickname;
+    io.emit('usersList', allUsers);
+  });
+
   socket.on('message', async ({ chatMessage, nickname }) => {
     const dbMsg = await Messages.create(chatMessage, nickname, currDate);
     io.emit('message', `${dbMsg.timestamp} ${dbMsg.nickname}: ${dbMsg.message}`);
   });
-  socket.on('users', async (user) => { io.emit('users', user); });
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', async (msg) => {
+    sendMsg(msg);
+  });
 });
 
 http.listen(3000);
