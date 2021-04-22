c2e873f8aab09c82855aff73f7831326ab41ed9b
Luiz Mello <luizmacmillan@gmail.com>
2021-04-22 14:48:49 -0300

    Criação da lista de usuários e timestamps das mensagens.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const express = require('express');
 const cors = require('cors');
 const httpServer = require('http');
@@ -18,21 +19,98 @@ app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/server.html`);
 });
 
+let users = [];
+let usedGuestNumbers = [];
+
+const generateTimeStamp = () => {
+  const dateOptions = Intl.DateTimeFormat(
+    'en-gb',
+    {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    },
+  ).formatToParts(new Date());
+  const dateParts = {};
+  dateOptions.forEach(({ type, value }) => {
+    dateParts[type] = value;
+  });
+  const { day, month, year, hour, minute, second, dayPeriod } = dateParts;
+  return `${day}/${month}/${year} ${hour}:${minute}:${second} ${dayPeriod}`;
+};
+
+const newGuestNumber = () => {
+  if (usedGuestNumbers.includes(1)) {
+    let newNumber = 2;
+    for (let i = 2; usedGuestNumbers.includes(i); i += 1) {
+      newNumber = i + 1;
+    }
+    usedGuestNumbers.push(newNumber);
+
+    return newNumber;
+  }
+  usedGuestNumbers.push(1);
+  return 1;
+};
+
+const getNick = (socketId) => {
+  const userFound = users.find(({ userId }) => socketId === userId);
+  return userFound.userNick;
+};
+
+const changeNick = (socketId, nick) => {
+  users.forEach((user, index) => {
+    if (socketId === user.userId) {
+      users[index].userNick = nick;
+    }
+  });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter(({ userId }) => socketId !== userId);
+};
+
 io.on('connection', (socket) => {
-  socket.on('disconnect', () => {
-    socket.broadcast.emit(
-      'serverMessage',
-      { message: 'Alguém acabou de se desconectar.' },
-    );
-  });
-  socket.on('message', (msg) => {
-    io.emit('serverMessage', { message: msg });
-  });
-  socket.emit('welcome', 'Seja bem vindo ao nosso Chat!');
+  const guestNumber = newGuestNumber();
+  users.push({ userId: socket.id, userNick: `Guest ${guestNumber}`, guestNumber });
+  const userNick = getNick(socket.id);
+  io.emit('updateUsers', users);
+  socket.emit('welcome', `Seja bem vindo ao nosso chat, ${userNick}!`);
   socket.broadcast.emit(
     'serverMessage',
-    { message: 'Alguém acabou de se conectar.' },
+    { message: `${userNick} acabou de se conectar.` },
   );
+
+  socket.on('disconnect', () => {
+    usedGuestNumbers = usedGuestNumbers.filter((number) => number !== guestNumber);
+    removeUser(socket.id);
+    io.emit('updateUsers', users);
+    socket.broadcast.emit(
+      'serverMessage',
+      { message: `${userNick} acabou de se desconectar.` },
+    );
+  });
+
+  socket.on('message', (msg) => {
+    io.emit('serverMessage', { message: `${generateTimeStamp()} - ${userNick}: ${msg}` });
+  });
+
+  socket.on('setNick', (nick) => {
+    changeNick(socket.id, nick);
+    io.emit('updateUsers', users);
+    socket.broadcast.emit(
+      'serverMessage',
+      { message: `${userNick} agora se chama ${nick}.` },
+    );
+    socket.emit(
+      'serverMessage',
+      { message: `Seu novo nick é: ${nick}.` },
+    );
+  });
 });
 
 http.listen(3000, () => {
