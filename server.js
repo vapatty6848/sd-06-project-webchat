268b8bfd6cc24505ce0cecbab2f19cbb76ea5a2a
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-04-21 21:12:51 -0300

    req 2 done

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 const cors = require('cors');
 const io = require('socket.io')(http, {
@@ -8,25 +10,42 @@ const io = require('socket.io')(http, {
   },
 });
 
-const { messageFormat } = require('./utils');
+const { messageFormat, randomNickname } = require('./utils');
 
 app.use(cors());
+
+// app.use('/views', express.static('views'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
+const sockets = [];
+
 io.on('connection', (socket) => {
   console.log(`${socket.id} conectou!`);
 
-  socket.emit('hello', 'Seja bem vindo(a)');
+  sockets.push({ id: socket.id, nickname: randomNickname() });
 
+  io.emit('onlineUsers', sockets);
+  // { id: 'Seja bem vindo(a)', randomNickname: randomNickname() }
   // socket.broadcast.emit('message', message({ chatMessage: 'se conectou', nickname: socket.id }));
-
-  socket.on('disconnect', () => console.log('Desconectado'));
-
+  
+  socket.on('disconnect', () => {
+    const userIndex = sockets.findIndex((user) => user.id === socket.id);
+    sockets.splice(userIndex, 1);
+    io.emit('onlineUsers', sockets);
+    console.log('Desconectado');
+  });
+  
   socket.on('message', ({ chatMessage, nickname }) => {
     io.emit('message', messageFormat({ chatMessage, nickname }));
+  });
+
+  socket.on('nickname', ({ nickname }) => {
+    const userIndex = sockets.findIndex((user) => user.id === socket.id);
+    sockets[userIndex].nickname = nickname;
+    io.emit('onlineUsers', sockets);
   });
 });
 
--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,18 @@
 const messageFormat = ({ chatMessage, nickname }) => {
   const date = new Date();
-  const ddmmyyyy = `${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`;
+  const formatedDay = `0${date.getDate()}`.slice(-2);
+  const formatedMonth = `0${date.getMonth() + 1}`.slice(-2);
+  const ddmmyyyy = `${formatedDay}-${formatedMonth}-${date.getFullYear()}`;
   const fullDate = `${ddmmyyyy} ${date.toLocaleTimeString()}`;
   return `${fullDate} - ${nickname}: ${chatMessage}`;
 };
 
+const randomNickname = () => {
+  const rand = () => Math.random().toString(36).replace('.', '');
+  return (rand() + rand()).substr(2, 16);
+};
+
 module.exports = {
   messageFormat,
+  randomNickname,
 };
