0e4370ab95804752f6fefcbec0c64561665eaf69
higoramorim <1810042@aluno.univesp.br>
2021-04-26 20:09:34 -0300

    reqs

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const moment = require('moment');
 
 const app = express();
-
 const http = require('http');
 
 const socketIoServer = http.createServer(app);
-
 const cors = require('cors');
-
 const socketIo = require('socket.io');
 
 const { getAllMsg, postMsg } = require('./models/chatModel');
 
-const checkDate = () => {
-  const date = new Date();
-  const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}
-  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  return formattedDate;
+// const checkDate = () => {
+//   const date = new Date();
+//   const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}
+//   ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+//   return formattedDate;
+// };
+
+const msgDate = () => {
+  const dateFormatted = moment().format('DD-MM-yyyy h:mm:ss A');
+  return dateFormatted;
 };
 
 const io = socketIo(socketIoServer, {
@@ -26,50 +29,48 @@ const io = socketIo(socketIoServer, {
   },
 });
 
-let users = [];
-const newUsersChat = ({ newUserNick, socket }) => {
-  const nickUser = users.map((user) => {
-  if (user.socketId !== socket.id) return user;
-   return { ...user, nickname: newUserNick };
-  });
-
-  users = nickUser;
-   io.emit('new users', { users });
-};
+const users = [];
 
 app.use(cors());
-
-const randomName = `user_${Math.random().toString().substr(2, 11)}`;
-io.on('connection', (socket) => {
-  const newUser = { socketId: socket.id, name: randomName };
-  const test = users.push(newUser);
-  console.log('test', test);
-  io.emit('new users', users);
-  socket.on('message', async ({ nickname, chatMessage }) => {
-    const newDate = checkDate();
-    await postMsg({ nickname, chatMessage, newDate });
-    const message = `${newDate}-${nickname}:${chatMessage}`;
-    io.emit('message', message);
-  });
-
-  socket.on('new users', (newNickName) => {
-    newUsersChat({ newNickName, socket });
-  });
-
-  socket.on('disconnect', () => {
-    const activeUsers = users.filter((user) => user.socketId !== socket.id);
-    users = activeUsers;
-    io.emit('new users', users);
-  });
-});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', async (_req, res) => {
-  const getAll = await getAllMsg();
+  const message = await getAllMsg();
 
-  res.render('home', { getAll, users });
+  res.render('home', { message, users });
+});
+
+const filterNickUser = ({ newNickname, socket }) => {
+  const index = users.findIndex((user) => user.id === socket.id);
+  users[index].nickname = newNickname;
+  console.log('users', users);
+  io.emit('updateUsers', users);
+};
+
+io.on('connection', async (socket) => {
+  socket.on('online-users', async (nickname) => {
+    users.push({ id: socket.id, nickname });
+    io.emit('updateUsers', users);
+  });
+
+  socket.on('message', async ({ nickname, chatMessage }) => {
+    const newDate = msgDate();
+    io.emit('message', `${newDate}-${nickname}:${chatMessage}`);
+    await postMsg({ nickname, chatMessage, newDate });
+  });
+
+  socket.on('updateNickname', (newNickname) => {
+    filterNickUser({ newNickname, socket });
+  });
+
+  socket.on('disconnect', () => {
+  console.log(`user ${socket.id} disconnected`);
+  const index = users.findIndex((user) => user.id === socket.id);
+  users.splice(index, 1);
+  io.emit('updateUsers', users);
+  });
 });
 
 socketIoServer.listen(3000, () => console.log('listening on port 3000'));
