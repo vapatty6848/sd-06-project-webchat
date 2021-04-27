280b5033a71bcb4e06b43e27c672e2248c7bd8a8
mottak <kissylamottapereira@gmail.com>
2021-04-27 17:10:36 -0300

    req 4

--- server.js
+++ server.js
@@ -53,29 +53,31 @@ const newMessage = (chatMessage, nickname) => `${getDate()} ${nickname} ${chatMe
 let userList = [];
 
 io.on('connection', (socket) => {
-  console.log(`usuário conectado ${socket.id}`);
   socket.on('connectedUser', async ({ id, nickname }) => {
-    io.emit('connectedUser', ({ id, nickname }));
     userList.push({ id, nickname });
+    io.emit('connectedUser', ({ id, nickname }));
+    io.emit('UsersList', (userList));
   });
   socket.on('newNickname', async ({ id, nickname }) => {
+    userList = filterUser(userList, id, nickname);
     io.emit('newNickname', { id, nickname });
-   userList = filterUser(userList, id, nickname);
+    io.emit('UsersList', (userList));
   });
   socket.on('message', async ({ chatMessage, nickname }) => {
     messages.createMessage({ message: chatMessage, nickname, timestamp: getDate() });
     io.emit('message', newMessage(chatMessage, nickname));
   });
+  
   socket.on('disconnect', () => {
-    console.log('usuário desconectado');
     userList = userList.filter((e) => e.id !== socket.id);
+    io.emit('UsersList', (userList));
   });
 });
 
 app.get('/', async (req, res) => {
   const allMessages = await messages.getAllMessages();
-  console.log(allMessages);
-  res.render('chat/index', { userList, allMessages });
+
+  res.render('chat/index', { allMessages });
 });
 
 http.listen(3000, () => console.log('Servidor na porta 3000'));
