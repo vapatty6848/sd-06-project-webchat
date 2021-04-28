7d440b8227a7b56d98959689a2ea12bbd6164cbd
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-04-28 20:25:43 -0300

    com muita ajuda do Sugano e do Mariotto consegui fazer o req1 e o req2 passarem (quer dozer, é o famoso 'na minha maquina funciona' )

--- server.js
+++ server.js
@@ -2,27 +2,45 @@ const express = require('express');
 
 const app = express();
 const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+const moment = require('moment');
 
 const PORT = process.env.PORT || 3000;
-
-const io = require('socket.io')(httpServer);
 
 app.set('view engine', 'ejs');
 app.set('views', './view');
 
 app.get('/', (_req, res) => {
-  res.render('home');
+  res.render('home', {}); 
 });
+
+let arrayUsers = [];
+
+const formatMessage = ({ chatMessage, nickname }) => {
+  const time = moment().format('DD-MM-YYYY HH:mm:ss A');
+  const message = `${time}  ${nickname} ${chatMessage}`;
+  io.emit('message', message);
+};
 
 io.on('connection', (socket) => {
   console.log(`Novo usuário conectado: ${socket.id}`);
   
-    socket.on('toBackMsg', (message) => {
-      socket.broadcast.emit('toFrontMsg', message);
+  socket.on('message', (message) => formatMessage(message));
+
+  socket.on('connectedUser', (nickname) => {
+    arrayUsers.push({ id: socket.id, nickname });
+    io.emit('nickname', arrayUsers);
   });
 
-  socket.on('nickname', (nickname) => {
-    io.emit('toFrontNick', nickname);
+  socket.on('updatedUser', (newNickname) => {
+    const chatUser = arrayUsers.find((user) => user.id === socket.id);
+    chatUser.nickname = newNickname;
+    arrayUsers = arrayUsers.map((user) => (user.id === socket.id ? chatUser : user));
+    io.emit('nickname', arrayUsers);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Usuário ${socket.id} desconectado`);
   });
 });
 httpServer.listen(PORT, () => console.log(`on PORT ${PORT}`));
