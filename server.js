332c1eb62b7d8f2cedce36e07b3a7782a938ec10
Alessandra <itsalematos@hotmail.com>
2021-04-28 17:48:32 -0400

    Feat: socket backend users exchange messages

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-// Faça seu código aqui 
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const { Http2ServerRequest } = require('http2');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    const { nickname, chatMessage } = msg;
+    
+    const date = new Date().toLocaleString();
+    let time = date.substring(13,11);
+
+    (parseInt(time) > 12) ? time = 'PM' : time = 'AM';
+    
+    const userMessage = `${date} ${time} - ${nickname}: ${chatMessage}`;
+    const formattedMsg = userMessage.replace(/\//g, '-');
+    console.log(formattedMsg);
+    io.emit('message', formattedMsg); // emite para todos
+    //socket.emit - apenas quem mandou recebe
+    // socket.broadcast - todos recebem, menos quem mandou
+  });
+
+ 
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+http.listen('3000');
