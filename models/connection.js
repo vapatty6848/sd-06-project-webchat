c5a36746ec45a2d92259a90bd398226bb7734dd8
vbergadev <vbergadev@gmail.com>
2021-04-28 15:53:35 -0300

    Req 1, ok local test

--- models/connection.js
+++ models/connection.js
@@ -1,16 +1,18 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
+const DB_NAME = process.env.DB_NAME || 'webchat';
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/webchat';
 let schema = null;
 
 async function connection() {
   if (schema) return Promise.resolve(schema);
   return MongoClient
-    .connect(process.env.DB_URL, {
+    .connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((conn) => conn.db(DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
@@ -20,5 +22,4 @@ async function connection() {
       process.exit(1);
     });
 }
-
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const HTTP = require('http').createServer(app);
+const cors = require('cors');
+const moment = require('moment');
+const io = require('socket.io')(HTTP, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+const { addMessages } = require('./models/messages');
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', async (_req, res) => {
+  res.render('../views/');
+});
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected.`);
+  socket.on('disconnect', () => console.log(`User ${socket.id} disconnected.`));
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const messageTime = moment().format('DD-MM-yyyy HH:mm:ss a'); // DD-MM-yyyy HH:mm:ss
+    console.log(messageTime);
+    await addMessages({ chatMessage, nickname, messageTime });
+    const result = `${messageTime} - ${nickname} - ${chatMessage}`;
+    io.emit('message', result);
+  });
+});
+
+HTTP.listen(3000, () => {
+  console.log('O pai ta ON na 3000');
+});
