b236b416f0f5aa5d42ef12625179cc90bde2e14e
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-28 13:42:30 -0300

    index server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 // Faça seu código aqui
+// codigo consultado no course
+
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
