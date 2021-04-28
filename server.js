a37784979badbd4321f2b627b6616727e754aa9a
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-04-28 18:54:37 -0300

    iniciando webChat

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('Alguém se conectou');
+
+  socket.on('disconnect', () => {
+    console.log('Alguém desconectou');
+  });
+
+  socket.on('mensagem', (msg) => {
+    console.log(`Alguém disse: ${msg}`);
+  });
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
