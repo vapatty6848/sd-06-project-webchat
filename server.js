1e103e14184a36c2c79c9d7445aa4c36f5d2f3eb
Alessandra <itsalematos@hotmail.com>
2021-04-29 17:25:32 -0400

    Fix: lint rules

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const cors = require('cors');
-const { Http2ServerRequest } = require('http2');
+// const { Http2ServerRequest } = require('http2');
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
-  }
+  },
 });
 
 app.use(cors());
@@ -16,24 +16,25 @@ io.on('connection', (socket) => {
     const { nickname, chatMessage } = msg;
     
     const date = new Date().toLocaleString();
-    let time = date.substring(13,11);
-
-    (parseInt(time) > 12) ? time = 'PM' : time = 'AM';
+    let time = date.substring(13, 11);
     
+    if (parseInt(time) > 12) {
+      time = 'PM';
+    } else {
+      time = 'AM';
+    }
+      
     const userMessage = `${date} ${time} - ${nickname}: ${chatMessage}`;
     const formattedMsg = userMessage.replace(/\//g, '-');
     console.log(formattedMsg);
     io.emit('message', formattedMsg); // emite para todos
-    //socket.emit - apenas quem mandou recebe
+    // socket.emit - apenas quem mandou recebe
     // socket.broadcast - todos recebem, menos quem mandou
   });
-
- 
 });
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 
 app.get('/', (_req, res) => {
   res.render('home');
