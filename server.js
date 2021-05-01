ac69a3a996df609037a53e7ca94dc08e56834d51
Marx Khristie Cavalheiro Pires Neves <marxkhristie@gmail.com>
2021-04-30 21:54:20 -0300

    Finalizando.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+// const express = require('express');
 const cors = require('cors');
 const app = require('express')();
 const http = require('http').createServer(app);
-const path = require('path');
+// const path = require('path');
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000',
