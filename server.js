52541e972c4351e011f2a77eb49d935918232397
deko possas <andrepossaster@gmail.com>
2021-05-02 17:24:07 -0300

    trocando as variaveis

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const http = require('http');
+const httpServer = require('http').createServer(app);
 
 const PORT = 3000;
 
-const io = require('socket.io')(http, {
+const io = require('socket.io')(httpServer, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
@@ -15,7 +16,11 @@ const io = require('socket.io')(http, {
 
 app.use(cors());
 
-let allUsers = [];
+app.set('view engine', 'views');
+app.set('view', path.join(__dirname, 'views'));
+app.use(express.statis(`${__dirname}/views/`));
+
+const allUsers = [];
 
 const addNewUser = ({ socket, nickname }) => {
   allUsers.push({ id: socket.id, nickname });
@@ -26,6 +31,6 @@ io.on('connection', async (socket) => {
   socket.on('newUser', ({ nickname }) => addNewUser({ socket, nickname }));
 });
 
-http.listen(3000, () => {
+httpServer.listen(PORT, () => {
   console.log(`Servindo na porta ${PORT}`);
 });
