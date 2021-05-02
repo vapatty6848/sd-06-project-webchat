f5f64a969cd9a5fdc3a1cb9dfa9c37af4696bd2f
deko possas <andrepossaster@gmail.com>
2021-05-02 15:14:51 -0300

    socket criado

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http');
+
+const PORT = 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+let allUsers = [];
+
+const addNewUser = ({ socket, nickname }) => {
+  allUsers.push({ id: socket.id, nickname });
+  io.emit('updatOnlineUsers', allUsers);
+};
+
+io.on('connection', async (socket) => {
+  socket.on('newUser', ({ nickname }) => addNewUser({ socket, nickname }));
+});
+
+http.listen(3000, () => {
+  console.log(`Servindo na porta ${PORT}`);
+});
