e9ab9cc11da0bbda8bd7e7eafec8cbda9471063e
deko possas <andrepossaster@gmail.com>
2021-05-02 14:15:34 -0300

    criei constantes de process

--- models/connection.js
+++ models/connection.js
@@ -2,15 +2,17 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 let schema = null;
+const DBURL = process.env.DB_URL || 'mongo';
+const DBNAME = process.env.DB_NAME || 'mongodb://localhost:27017/webchat';
 
 async function connection() {
   if (schema) return Promise.resolve(schema);
   return MongoClient
-    .connect(process.env.DB_URL, {
+    .connect(DBURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((conn) => conn.db(DBNAME))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
