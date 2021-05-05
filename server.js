11b1e9f86fdd63d3d03b9aec3e6013df2f30e060
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-05-05 19:10:30 -0300

    finally

--- server.js
+++ server.js
@@ -27,38 +27,35 @@ const onConnect = async (socket) => {
    `${message.date} - ${message.nickname}: ${message.message}`)); 
 };
 
-const nickUpdate = async (socket) => {
- socket.on('nickNameUpdate', (nickName) => {
+const nickUpdate = async (nickName, socket) => {
+  console.log('entrei no nickUpdate');
   const currentUser = users.find((usr) => usr.id === socket.id);
   const userIndex = users.indexOf(currentUser);
   users.splice(userIndex, 1, { id: socket.id, nickName });
-  io.emit('nickNameUpdateFront', users);
-  }); 
+  io.emit('nickNameUpdateFront', users); 
 };
 
-const messageProcess = async (socket) => {
-  socket.on('message', async (message) => {
-    const { nickname, chatMessage } = message;
-    io.emit('message', `${dateTime} - ${nickname}: ${chatMessage}`);
-    Messages.createMessage(nickname, chatMessage, dateTime);
-  });
+const messageProcess = async (socket, message) => {
+  console.log('entrei no messageProcess');
+
+  const { nickname, chatMessage } = message;
+  io.emit('message', `${dateTime} - ${nickname}: ${chatMessage}`);
+  Messages.createMessage(nickname, chatMessage, dateTime);
 };
 
 const onDisconnect = async (socket) => {
-  socket.on('disconnect', () => {
-    const currentUser = users.find((usr) => usr.id === socket.id);
-    const userIndex = users.indexOf(currentUser);
-    users.splice(userIndex, 1);
-    io.emit('nickNameUpdateFront', users);
-    console.log('entrei no onDisconnect');
-    console.log(users);
-  });
+  const currentUser = users.find((usr) => usr.id === socket.id);
+  const userIndex = users.indexOf(currentUser);
+  users.splice(userIndex, 1);
+  io.emit('nickNameUpdateFront', users);
+  console.log('entrei no onDisconnect');
+  console.log(users);
 };
 io.on('connection', async (socket) => {
   await onConnect(socket);
-  await nickUpdate(socket);
-  await messageProcess(socket);
-  await onDisconnect(socket);
+  socket.on('nickNameUpdate', (nickName) => nickUpdate(nickName, socket));
+  socket.on('message', async (message) => messageProcess(socket, message));
+  socket.on('disconnect', () => onDisconnect(socket));
 });
 
 httpServer.listen('3000');
