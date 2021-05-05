37b4ef0dee0e09c85bf7a1b55d5b8f98af29d119
Ana Higo <anahigo.arq@gmail.com>
2021-05-05 17:32:49 -0300

    Requisito 1, teste local OK, Lint OK

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const dateFormat = require('dateformat');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static(`${__dirname}/public/`));
+
+io.on('connection', (socket) => {
+  console.log(`usuário conectado ${socket.id}`);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const dateAndTime = dateFormat(new Date(), 'dd-mm-yyyy h:MM:ss TT');
+    const theNewMessage = `${dateAndTime} ${nickname} ${chatMessage}`;
+    io.emit('message', theNewMessage);
+  });
+});
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+http.listen(3000, () => console.log('Servidor na porta 3000'));
