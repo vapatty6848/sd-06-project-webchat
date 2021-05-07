188bee2669269585ac71b44b83dd8cbe33f6e1f2
labelle <beelsdelonge@gmail.com>
2021-05-06 21:27:49 -0300

    Arrumando ultimo erro de Lint primeiro requisito

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ io.on('connection', (socket) => {
 
   socket.on('message', ({ nickname, chatMessage }) => 
     io.emit('message', `${fullData} - ${nickname} ${chatMessage}`));
-  
 });
 
 httpServer.listen(3000, () => {
