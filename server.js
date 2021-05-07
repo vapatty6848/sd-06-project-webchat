3d3105089ccc3ef2402ac071cd8d3d551dfc2c59
Ana Higo <anahigo.arq@gmail.com>
2021-05-07 19:39:39 -0300

    2 ainda nao esta comleto

--- server.js
+++ server.js
@@ -2,33 +2,59 @@ const express = require('express');
 const cors = require('cors');
 const dateFormat = require('dateformat');
 
+const PORT = 3000;
 const app = express();
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
-    cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST'],
-    },
-  });
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static(`${__dirname}/public/`));
 
+const usersList = [];
+
+const findUser = (listNickname, id, nickname) => {
+  const detectUser = listNickname.findIndex((user) => user.id === id);
+  listNickname.splice(detectUser, 1, { id, nickname });
+    return listNickname;
+};
+
+const dateAndTime = dateFormat(new Date(), 'dd-mm-yyyy h:MM:ss TT');
+
 io.on('connection', (socket) => {
+  // console.log('socket', socket);
   console.log(`usuário conectado ${socket.id}`);
 
+  socket.on('connectedUsers', ({ id, nickname }) => {
+    usersList.push({ id, nickname });
+    console.log(usersList);
+    io.emit('connectedUsers', (usersList));
+  });
+
+  socket.on('nickname', ({ nick }) => {
+    const userList = findUser(usersList, socket.id, nick);
+    console.log(userList);
+    io.emit('nickname', (userList));
+  });
+
   socket.on('message', ({ chatMessage, nickname }) => {
-    const dateAndTime = dateFormat(new Date(), 'dd-mm-yyyy h:MM:ss TT');
-    const theNewMessage = `${dateAndTime} ${nickname} ${chatMessage}`;
-    io.emit('message', theNewMessage);
+    const newMessage = `${dateAndTime} - ${nickname}: ${chatMessage}`;
+    // console.log(newMessage);
+    io.emit('message', newMessage);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`usuário desconectado ${socket.id}`);
   });
 });
 
-app.get('/', (_req, res) => {
-  res.render('home');
-});
+app.get('/', async (_req, res) => res.render('home'));
 
-http.listen(3000, () => console.log('Servidor na porta 3000'));
+http.listen(PORT, () => console.log('App listening on PORT %s', PORT));
