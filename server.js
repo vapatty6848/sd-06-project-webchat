2c78f9111e61b955febdd83bf71192a42fe2b1dc
Mario Duarte <mario.duarte@g.globo>
2021-05-08 14:11:30 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// Faça seu código aqui
+const app = require('express')();
+
+const http = require('http').createServer(app);
+
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const { formatDate } = require('./functions');
+
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  console.log('Conectado');
+  socket.on('message', (message) => {
+    console.log('Mensagem enviada');
+    io.emit('messageData', `${formatDate()} - ${message.nickname}: ${message.chatMessage}`);
+  });
+  socket.on('disconnect', () => {
+    console.log('Desconectado');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
