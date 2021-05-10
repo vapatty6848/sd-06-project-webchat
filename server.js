f31be9350cc349e904808052ff36fce50d6a6ead
VinicinBraga <vinicius@bragamatta.com.br>
2021-05-10 11:10:47 -0300

    Iniciando de acordo com a aula

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 // Faça seu código aqui.
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(httpServer);
+
+io.on('connection', (socket) => {
+  console.log(`Usuario novo conectado: ${socket.id}`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './view');
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+httpServer.listen(PORT, () => console.log(`on PORT ${PORT}`));
