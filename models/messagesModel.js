1bf3d0abe62e9debcff624744d018d4c40ab8021
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-05-14 14:29:18 -0300

    god save us all 2

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -8,36 +8,37 @@ const saveMessage = async (message, nickname, timestamp) => {
 };
 
 const getMessages = async () => {
-  const messages = await connection().then((db) => db.collection('messages')
+  const messages = await connection().then((db) => db.collection(collection)
     .find().toArray());
 
   return messages;
 };
 
-const getUsers = async () => {
-  const users = await connection().then((db) => db.collection('users')
-    .find().toArray());
+// const getUsers = async () => {
+//   const users = await connection().then((db) => db.collection('users')
+//     .find().toArray());
+//   return users;
+// };
 
-  return users;
-};
+// const saveUser = async (nickname) => {
+//   await connection().then((db) => 
+//     db.collection('users').insertOne({ nickname }));
+// };
 
-const saveUser = async (nickname) => {
-  await connection().then((db) => 
-    db.collection(collection).insertOne({ nickname }));
-};
+// const deleteUser = async (nickname) => {
+//   await connection().then((db) => 
+//     db.collection('users').deleteOne({ nickname }));
+// };
 
-const updateUser = async (oldNickname, newNickname) => {
-  await connection().then((db) => 
-    db.collection(collection).updateOne(
-      { nickname: oldNickname },
-      { $set: { nickname: newNickname } },
-    ));
-};
+// const updateUser = async (oldNickname, newNickname) => {
+//   await connection().then((db) => 
+//     db.collection('users').updateOne(
+//       { nickname: oldNickname },
+//       { $set: { nickname: newNickname } },
+//     ));
+// };
 
 module.exports = {
   saveMessage,
   getMessages,
-  saveUser,
-  updateUser,
-  getUsers,
 };
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// Faça seu código aqui
 const express = require('express');
-const { saveMessage, getMessages, getUsers } = require('./models/messagesModel');
 
 const app = express();
+
 const httpServer = require('http').createServer(app);
 
 const io = require('socket.io')(httpServer, {
@@ -12,61 +11,73 @@ const io = require('socket.io')(httpServer, {
   },
 });
 
+const {
+  saveMessage,
+  getMessages,
+  } = require('./models/messagesModel');
+
 app.set('view engine', 'ejs');
 
 app.use('/', express.static(__dirname));
 
 const users = [];
 
-io.on('connection', (socket) => {
-  const now = new Date();
-  const date = `${now.getDate()}-${(now.getMonth() + 1)}-${now.getFullYear()}`;
-  const time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-  console.log(`${socket.id} logged in at ${date} ${time}`);
-  users.push({
-    id: socket.id,
-    nickname: socket.id.slice(0, 16),
-  });
-  io.emit('name', socket.id);
-  socket.on('disconnect', () => {
-    console.log('Someone logged off');
-    users.splice(users.indexOf(socket.id), 1);
-    console.log(users);
-  });
-  socket.on('list', () => {
-    const arrayNickname = users.map((user) => user.nickname);
-    io.emit('list', arrayNickname);
-  })
-  socket.on('message', ({ chatMessage, nickname }) => {
-    console.log('oi');
+const now = new Date();
+const date = `${now.getDate()}-${(now.getMonth() + 1)}-${now.getFullYear()}`;
+const time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+const socketOnDisconnect = (socket) => {
+  socket.on('disconnect', async () => {
     users.forEach((user) => {
-      if(user.id === socket.id && user.nickname === socket.id.slice(0, 16)) {
+      if (user.id === socket.id) {
+        io.emit('removeUser', user.nickname);
+      }
+    });
+    users.splice(users.indexOf(socket.id), 1);
+  });
+};
+
+const socketOnMessage = (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    users.forEach((user) => {
+      if (user.id === socket.id && user.nickname === socket.id.slice(0, 16)) {
         user.nickname = nickname;
       }
     });
     users.forEach(async (user) => {
-      if(user.id === socket.id) {
+      if (user.id === socket.id) {
         io.emit('message', `${date} ${time} - ${user.nickname}: ${chatMessage}`);
         await saveMessage(chatMessage, user.nickname, `${date} ${time}`);
       }
     });
-    
   });
+};
+
+io.on('connection', async (socket) => {
+  users.push({ id: socket.id, nickname: socket.id.slice(0, 16)});
+  io.emit('name', socket.id);
+  users.forEach((user) => {
+    if (user.id === socket.id) io.emit('addUser', user.nickname);
+  });
+  const listOfNicknames = users.map((user) => user.nickname);
+  io.emit('listUpdate', listOfNicknames);
+  socketOnDisconnect(socket);
+  socketOnMessage(socket);
   socket.on('changeNick', (nickname) => {
-    console.log('aqui')
     users.forEach((user) => {
-      if(user.id === socket.id) {
-        user.nickname = nickname;
+      if (user.id === socket.id) {
+        io.emit('removeUser', user.nickname);
+        io.emit('addUser', nickname);
+        user.nickname = nickname; 
       }
     });
-    console.log(users);
-  })
+  });
 });
 
 app.get('/', async (req, res) => {
   const savedMessages = await getMessages();
-  const savedUsers = await getUsers();
-  res.render('home/index', { savedMessages, savedUsers });
+  // const savedUsers = await getUsers();
+  res.render('home/index', { savedMessages });
 });
 
 httpServer.listen(3000, () => console.log('Lister in port 3000'));
