aea75b4853be9f058acd97cde03b52a73c578f98
Thiago <asm.thiago@gmail.com>
2021-05-18 21:46:17 -0300

    Server transmitindo nickname e users

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-// Faça seu código aqui
+const cors = require('cors');
+const express = require('express');
+const moment = require('moment');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+app.use(cors());
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const users = [];
+const messageDate = moment().format('DD-MM-YYYY hh:mm:ss A');
+
+const randomNickname = () => {
+  const length = 16;
+  const characteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i += 1) {
+    result += characteres.charAt(Math.floor(Math.random() * characteres.length));
+  }
+  return result;
+};
+
+io.on('connection', (socket) => {
+  socket.on('connected', (nickname) => {
+      users.push({nickname, sokectId: socket.id});
+  })
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const editedMessage = `${messageDate} - ${nickname}: ${chatMessage}`;
+    io.emit('message', editedMessage);
+  });
+});
+
+app.set('view engine', 'ejs'); //Cria uma view do lado do servidor, usando um ejs que é um view engine(motor de criação);
+app.set('views', './views');
+
+app.get('/', (_req, res) => {
+  res.render('home',{nickname: randomNickname(), users });
+});
+
+httpServer.listen('3000');
