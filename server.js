0c8d58089048621f2646dca8bccabf810d4d49f4
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-05-19 20:41:27 -0300

    setting up for first req

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-// Faça seu código aqui
+require('dotenv/config');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+let USERS = [];
+
+io.on('connection', (socket) => {
+  socket.on('newUser', (nickname) => {
+    USERS.push({ id: socket.id, nickname });
+    io.emit('updateUsers', USERS)
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Lintening on port ${PORT}`)
+});
