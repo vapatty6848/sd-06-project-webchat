89b0d22870cd41e5c5471043ed6e542c486b1b98
Pedro Eduardo <pedroedu77@gmail.com>
2021-05-21 19:20:06 -0300

    Requisito 1

---
+++ models/messageModel.js
@@ -0,0 +1,12 @@
+const connection = require('./connection');
+
+const addMessages = async ({ chatMessage, nickname, date }) => connection()
+  .then(async (db) => {
+    const addMessage = await db.collection('messages').insertOne({chatMessage, nickname, date});
+    // Aplicando o valor padrão as opções da função 
+    return addMessage.ops[0];
+  });
+
+module.exports = {
+  addMessages,
+};
--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-// Faça seu código aqui
+const express = require('express');
+
+const cors = require('cors');
+const path = require('path');
+const moment = require('moment');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { 
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+const { addMessages } = require('./models/messageModel');
+
+app.use(cors());
+app.use(express.urlencoded({
+  extended: true }));
+app.use(express.static(path.join(__dirname, 'views ')));
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+io.on('connection', async (socket) => {
+  socket.on('disconnect', () => {
+    console.log('o Lendário desconectou!');
+  });
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const messageFormat = moment().format('DD-MM-yyyy HH:mm:ss A');
+    const result = `${messageFormat} - ${nickname} : ${chatMessage}`;
+    io.emit('message', result);
+  });
+});
+
+app.get('/', async (_req, res) => {
+  res.render('../views')
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
