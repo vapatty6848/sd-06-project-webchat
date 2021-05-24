4bcaf01354da81e749b88b2478f4ab7666b871d5
SamuelPPlab <samuelpp.s@hotmail.com>
2021-05-24 20:06:16 -0300

    encerrando por hoje, quase pronto o requisito 2

--- server.js
+++ server.js
@@ -7,15 +7,25 @@ const port = 3000;
 
 const io = require('socket.io')(httpServer);
 const date = () => moment().format('DD-MM-YYYY hh:mm:ss A');
+let sockets = [];
 
 io.on('connection', (socket) => {
-	console.log(socket.id)
-
 	socket.on('message', (data) => {
 		const { nickname, chatMessage } = data;
-		
 		const message = `${date()} - ${nickname}: ${chatMessage}`;
 		io.emit('message', message)
+		sockets.push({ nickname, userId: socket.id });
+	});
+	socket.on('newNickname', (nickname) => {
+		socket.nickname = nickname;
+	});
+	socket.on('createNickname', (nickname) => {
+		sockets.push({ nickname, userId: socket.id });
+		io.emit('newUserList', sockets);
+	});
+	socket.on('disconnect', () => {
+		sockets = sockets.filter((item) => item.userId !== socket.id);
+		io.emit('newUserList', sockets)
 	})
 });
 
@@ -23,7 +33,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', (_req, res) => {
-	res.render('home');
+	res.render('home', { sockets });
 });
 
 httpServer.listen(port);
