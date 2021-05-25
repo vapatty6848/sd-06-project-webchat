f6f01f391e60ea3d0aaaab6a18d80360e9ea6b09
SamuelPPlab <samuelpp.s@hotmail.com>
2021-05-25 17:03:59 -0300

    Lint

--- models/messageController.js
+++ models/messageController.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 const createMessage = ({ message, nickname, timestamp }) => {
   connection().then((db) => (
     db.collection('messages').insertOne({ message, nickname, timestamp })
-  ))
+  ));
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,45 +1,52 @@
 // Faça seu código aqui
 const moment = require('moment');
 const express = require('express');
+
 const app = express();
 const httpServer = require('http').createServer(app);
+
 const port = 3000;
 
 const io = require('socket.io')(httpServer);
+
 const date = () => moment().format('DD-MM-YYYY hh:mm:ss A');
 let sockets = [];
 
+const nicknameUpdater = (oldNickname, newNickname) => {
+  sockets.forEach((item, i) => {
+    const { userId } = item;
+    if (item.nickname === oldNickname) {
+      sockets[i] = { userId, nickname: newNickname };
+    }
+  });
+};
+
 io.on('connection', (socket) => {
-	socket.on('message', (data) => {
-		const { nickname, chatMessage } = data;
-		const message = `${date()} - ${nickname}: ${chatMessage}`;
-		io.emit('message', message)
-		sockets.push({ nickname, userId: socket.id });
-	});
-	socket.on('newNickname', ({ newNickname, oldNickname }) => {
-		const newUserList = sockets.map((item) => {
-			if (item.nickname === oldNickname) {
-				item.nickname = newNickname;
-			}
-			return item;
-		});
-		io.emit('newUserList', sockets);
-	});
-	socket.on('createNickname', (nickname) => {
-		sockets.push({ nickname, userId: socket.id });
-		io.emit('newUserList', sockets);
-	});
-	socket.on('disconnect', () => {
-		sockets = sockets.filter((item) => item.userId !== socket.id);
-		io.emit('newUserList', sockets);
-	})
+  socket.on('message', (data) => {
+    const { nickname, chatMessage } = data;
+    const message = `${date()} - ${nickname}: ${chatMessage}`;
+    io.emit('message', message);
+    sockets.push({ nickname, userId: socket.id });
+  });
+  socket.on('newNickname', ({ newNickname, oldNickname }) => {
+    nicknameUpdater(oldNickname, newNickname);
+    io.emit('newUserList', sockets);
+  });
+  socket.on('createNickname', (nickname) => {
+    sockets.push({ nickname, userId: socket.id });
+    io.emit('newUserList', sockets);
+  });
+  socket.on('disconnect', () => {
+    sockets = sockets.filter((item) => item.userId !== socket.id);
+    io.emit('newUserList', sockets);
+  });
 });
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', (_req, res) => {
-	res.render('home');
+  res.render('home');
 });
 
 httpServer.listen(port);
